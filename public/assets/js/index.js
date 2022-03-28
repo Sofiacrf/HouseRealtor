3e3f503e313fceb2d2c14c77e169691fd588de68
sofiacrf <sofia.rojasflo@gmail.com>
2022-03-28 17:31:27 -0600

    finished api functions

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -6,25 +6,62 @@ const newsApiKey = '7f69ce2de7f443b8b756ea57b6f8fcad';
 const newsContainer = document.querySelector('#news');
 
 // Function to render the news card
-function renderNewsCard (articles) {
+function renderNewsCard(articles) {
     console.log(articles);
+    // Set the data API variables
+    const newsTitle = articles.title;
+    const newsContent = articles.description;
+    const newsSource = articles.source.name;
+    const newsURL = articles.url;
+
+    // Create elements for a card
+    const col = document.createElement('div');
+    const card = document.createElement('div');
+    const cardBody = document.createElement('div');
+    const titleEl = document.createElement('h5');
+    const contentEl = document.createElement('p');
+    const sourceEl = document.createElement('p');
+    const urlEl = document.createElement('link');
+
+    // Append elements
+    col.append(card);
+    card.append(cardBody);
+    cardBody.append(titleEl, contentEl, sourceEl, urlEl);
+
+    // Setting Attributes
+    col.setAttribute('class', 'col-md');
+    col.classList.add('five-day-card');
+    card.setAttribute('class', 'card bg-primary h-100 text-white');
+    cardBody.setAttribute('class', 'card-body p-2');
+    titleEl.setAttribute('class', 'card-title');
+    contentEl.setAttribute('class', 'card-text');
+    sourceEl.setAttribute('class', 'card-text');
+    urlEl.setAttribute('class', 'card-text');
+
+    // Add content to elements
+    titleEl.textContent = `${newsTitle}`;
+    contentEl.textContent = `${newsContent}`;
+    sourceEl.textContent = `${newsSource}`;
+    urlEl.textContent = `${newsURL}`;
+
+    newsContainer.append(col);
 }
 
 // Function to display the news
 function renderNews(articles) {
-   let headingCol = document.createElement('div');
-   let heading = document.createElement('h4');
+    const headingCol = document.createElement('div');
+    const heading = document.createElement('h4');
+ 
+    headingCol.setAttribute('class', 'col-12');
+    heading.textContent = 'Top News';
+    headingCol.append(heading);
 
-   headingCol.setAttribute('class', 'col-12');
-   heading.textContent = 'Top News';
-   headingCol.append(heading);
+    newsContainer.innerHTML = '';
+    newsContainer.append(headingCol);
 
-   newsContainer.innerHTML = '';
-   newsContainer.append(headingCol);
-
-  for (let i = 0; i < 5; i++) {
-      renderNewsCard(articles[i]);
-  }
+    for (let i = 0; i < 5; i++) {
+        renderNewsCard(articles[i]);
+    }
 }
 
 // Function to render the API articles
@@ -33,17 +70,17 @@ function renderItems(data) {
 }
 
 // Function to fetch the API data
-function fetchNews () {
-const country = 'us';
-const category = 'business';
-const apiURL = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${newsApiKey}`;
+function fetchNews() {
+    const country = 'us';
+    const category = 'business';
+    const apiURL = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${newsApiKey}`;
 
-fetch(apiURL)
-.then(res => res.json())
-.then(function (data) {
-    renderItems(data);
-})
-.catch(err => console.log(err))
+    fetch(apiURL)
+        .then(res => res.json())
+        .then(function (data) {
+            renderItems(data);
+        })
+        .catch(err => console.log(err))
 }
 
 fetchNews();
